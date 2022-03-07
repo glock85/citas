4e8fa6bda639bd8ff46c1a17c973b1e5f26dbfd0
Joseph López <gerardomarcanoopsu2016@gmail.com>
2022-03-07 19:30:15 -0400

    app-citas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState, useEffect} from 'react';
+import Formulario from './components/Formulario';
+import Cita from './components/Cita';
+import PropTypes from 'prop-types';
 
 function App() {
+
+  //Citas en LocalStorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales) {
+    citasIniciales = [];
+  }
+
+  //Arreglo de citas
+
+  const [citas, guardarCitas] = useState (citasIniciales);
+
+  //Use Effect para realizar ciertas operaciones cuando el state cambia
+  useEffect ( () => {
+    let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+    if(citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas));
+    }
+    else{
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  });
+
+  //Funcion que tome las citas actuales y agregue la nueva
+  const crearCita = cita => { guardarCitas([...citas, cita]); }
+
+  //Funcion que elimina una cita por su id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
+  //Mensaje condicional
+
+  const titulo = citas.length === 0 ? 'No hay citas, por favor agrega una.' : 'Administra tus Citas';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Fragment>
+
+      <h1>Administrador de Paciente</h1>
+
+      <div className="container">
+
+        <div className='one-half column'>
+
+          <Formulario crearCita={crearCita}/>
+
+        </div>
+
+        <div className='one-half column'>
+
+            <h2>{titulo}</h2>
+
+            {citas.map(cita => (<Cita key={cita.id} cita={cita} eliminarCita={eliminarCita}/>))};
+
+        </div>
+
+      </div>
+
+    </Fragment>
+  
   );
+}
+
+Formulario.propTypes = {
+  crearCita: PropTypes.func.isRequired
 }
 
 export default App;
---
+++ src/components/Cita.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+
+const Cita = ({cita, eliminarCita}) => ( 
+
+    <div className='cita'>
+
+        <p>Mascota: <span>{cita.mascota}</span></p>
+        <p>propietario: <span>{cita.propietario}</span></p>
+        <p>fecha: <span>{cita.fecha}</span></p>
+        <p>hora: <span>{cita.hora}</span></p>
+        <p>sintomas: <span>{cita.sintomas}</span></p>
+
+        <button className='button eliminar u-full-width' onClick={ () => eliminarCita(cita.id) } >Eliminar Cita &times;</button>
+
+    </div>
+ );
+
+ Cita.propTypes = {
+     cita: PropTypes.object.isRequired,
+     eliminarCita: PropTypes.func.isRequired
+ }
+export default Cita;
